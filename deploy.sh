4989224b0ab6b47152f8528422b923cd7d524e13
Yaroslav S <angrynaz@gmail.com>
2021-02-01 22:29:19 +0000

    fixed wierd Gradle cleaning after build instead of before

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-./gradlew ktlintFormat clean build -x test &&
+./gradlew clean && 
+./gradlew ktlintFormat build -x test &&
 docker build . -f Dockerfile -t angrynaz/familybot &&
 docker push angrynaz/familybot &&
 ssh root@storozhenko.dev '/root/familybot.sh'
