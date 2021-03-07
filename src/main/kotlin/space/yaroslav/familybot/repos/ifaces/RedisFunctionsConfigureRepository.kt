ccdb6436ed3d6b6c5090f1a5c0e5f54cb4c09247
Yaroslav <angrynaz@gmail.com>
2021-03-07 10:51:05 +0300

    fallback to old changes

--- src/main/kotlin/space/yaroslav/familybot/repos/ifaces/RedisFunctionsConfigureRepository.kt
+++ src/main/kotlin/space/yaroslav/familybot/repos/ifaces/RedisFunctionsConfigureRepository.kt
@@ -5,16 +5,25 @@ import org.springframework.stereotype.Component
 import space.yaroslav.familybot.common.Chat
 import space.yaroslav.familybot.common.utils.key
 import space.yaroslav.familybot.models.FunctionId
+import space.yaroslav.familybot.repos.PostgresFunctionsConfigureRepository
 import space.yaroslav.familybot.services.settings.EasySettingsRedisRepository
 
 @Component
 @Primary
 class RedisFunctionsConfigureRepository(
-    private val settingsRepository: EasySettingsRedisRepository
+    private val settingsRepository: EasySettingsRedisRepository,
+    private val oldRepository: PostgresFunctionsConfigureRepository
 ) : FunctionsConfigureRepository {
 
     override fun isEnabled(id: FunctionId, chat: Chat): Boolean {
-        return settingsRepository.get(id.easySetting, chat.key()) ?: true
+        val redisFunctionState = settingsRepository.get(id.easySetting, chat.key())
+        return if (redisFunctionState == null) {
+            val postgresFunctionState = oldRepository.isEnabled(id, chat)
+            settingsRepository.put(id.easySetting, chat.key(), postgresFunctionState)
+            postgresFunctionState
+        } else {
+            redisFunctionState
+        }
     }
 
     override fun switch(id: FunctionId, chat: Chat) {
