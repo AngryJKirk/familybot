e55366386bedb09c3d7f957fa60b30e1fe27bddc
Yaroslav S <angrynaz@gmail.com>
2021-03-07 08:01:10 +0000

    improved deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-
-./gradlew clean && 
+./gradlew clean &&
 ./gradlew ktlintFormat build -x test &&
 docker build . -f Dockerfile -t angrynaz/familybot &&
-docker push angrynaz/familybot &&
-ssh root@storozhenko.dev '/root/familybot.sh'
+docker rm -f suchara
+echo 'Bot has been stopped' &&
+docker run -d --restart always --net suchara-net -p 127.0.0.1:8080:8080 --name suchara angrynaz/familybot:latest &&
+echo 'Everything is done, master'
