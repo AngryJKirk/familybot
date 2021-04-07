5b3544885c6b84b69ded902604e9dd338b88cd10
Yaroslav <angrynaz@gmail.com>
2021-04-07 19:49:49 +0300

    completly changed deploy flow

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-docker build . -f Dockerfile -t angrynaz/familybot &&
-docker rm -f suchara
-echo 'Bot has been stopped' &&
-docker run -d --restart always --net suchara-net -p 127.0.0.1:8080:8080 --name suchara angrynaz/familybot:latest &&
-echo 'Everything is done, master'
