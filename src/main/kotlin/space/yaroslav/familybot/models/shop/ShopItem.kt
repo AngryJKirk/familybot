272c8a9444fd4d23b242d06303cdf5df4299233d
Yaroslav <github@storozhenko.dev>
2021-06-04 16:06:10 +0300

    wow, sale

--- src/main/kotlin/space/yaroslav/familybot/models/shop/ShopItem.kt
+++ src/main/kotlin/space/yaroslav/familybot/models/shop/ShopItem.kt
@@ -8,10 +8,10 @@ import space.yaroslav.familybot.services.talking.DictionaryContext
 
 enum class ShopItem(val title: Phrase, val description: Phrase, val price: Int) {
 
-    DROP_PIDOR_LIMIT(Phrase.DROP_PIDOR_LIMIT_TITLE, Phrase.DROP_PIDOR_LIMIT_DESC, 100.rubles()),
-    DROP_BET_LIMIT(Phrase.DROP_BET_LIMIT_TITLE, Phrase.DROP_BET_LIMIT_DESC, 200.rubles()),
-    DROP_PIDOR(Phrase.DROP_PIDOR_TITLE, Phrase.DROP_PIDOR_DESC, 300.rubles()),
-    DROP_ASK_WORLD_LIMIT(Phrase.DROP_ASK_WORLD_LIMIT_TITLE, Phrase.DROP_ASK_WORLD_LIMIT_DESC, 400.rubles());
+    DROP_PIDOR_LIMIT(Phrase.DROP_PIDOR_LIMIT_TITLE, Phrase.DROP_PIDOR_LIMIT_DESC, 50.rubles()),
+    DROP_BET_LIMIT(Phrase.DROP_BET_LIMIT_TITLE, Phrase.DROP_BET_LIMIT_DESC, 100.rubles()),
+    DROP_PIDOR(Phrase.DROP_PIDOR_TITLE, Phrase.DROP_PIDOR_DESC, 150.rubles()),
+    DROP_ASK_WORLD_LIMIT(Phrase.DROP_ASK_WORLD_LIMIT_TITLE, Phrase.DROP_ASK_WORLD_LIMIT_DESC, 200.rubles());
 
     companion object {
         fun toKeyBoard(dictionaryContext: DictionaryContext): InlineKeyboardMarkup {
