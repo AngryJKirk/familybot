787cce9f3ddbe3f20208bb114deed1587e39252d
Yaroslav <github@storozhenko.dev>
2021-10-30 19:40:10 +0300

    Automated command registration

--- src/main/kotlin/space/yaroslav/familybot/telegram/BotStarter.kt
+++ src/main/kotlin/space/yaroslav/familybot/telegram/BotStarter.kt
@@ -5,7 +5,13 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
 import org.springframework.context.event.EventListener
 import org.telegram.telegrambots.meta.TelegramBotsApi
+import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands
+import org.telegram.telegrambots.meta.api.objects.commands.BotCommand
+import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeAllGroupChats
+import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeAllPrivateChats
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession
+import org.tomlj.Toml
+import org.tomlj.TomlParseResult
 
 @Configuration
 @Profile(BotStarter.NOT_TESTING_PROFILE_NAME)
@@ -16,9 +22,48 @@ class BotStarter {
         const val NOT_TESTING_PROFILE_NAME = "!$TESTING_PROFILE_NAME"
     }
 
+    private val commands: List<BotCommand>
+    private val helpCommand: BotCommand
+
+    init {
+        val resourceAsStream = this::class.java.classLoader
+            .getResourceAsStream("static/commands.toml")
+            ?: throw FamilyBot.InternalException("commands.toml is missing")
+
+        val toml = Toml.parse(resourceAsStream)
+
+        helpCommand = BotCommand("help", extractValue(toml, "help"))
+        commands = toml
+            .keySet()
+            .sortedBy { key -> toml.inputPositionOf(key)?.line() }
+            .map { key -> key to extractValue(toml, key) }
+            .map { (command, description) -> BotCommand(command, description) }
+            .toList()
+    }
+
     @EventListener(ApplicationReadyEvent::class)
     fun telegramBot(event: ApplicationReadyEvent) {
         val telegramBotsApi = TelegramBotsApi(DefaultBotSession::class.java)
-        telegramBotsApi.registerBot(event.applicationContext.getBean(FamilyBot::class.java))
+        val bot = event.applicationContext.getBean(FamilyBot::class.java)
+        telegramBotsApi.registerBot(bot)
+        bot.execute(
+            SetMyCommands
+                .builder()
+                .commands(commands)
+                .scope(BotCommandScopeAllGroupChats())
+                .build()
+        )
+        bot.execute(
+            SetMyCommands
+                .builder()
+                .command(helpCommand)
+                .scope(BotCommandScopeAllPrivateChats())
+                .build()
+        )
+    }
+
+    private fun extractValue(toml: TomlParseResult, key: String): String {
+        return toml.getString(key)
+            ?: throw FamilyBot.InternalException("Missing command description for key=$key")
     }
 }
