536b8600a6354975cbc1f0ea0e22c20c78b7a092
Yaroslav Storozhenko <yaroslav@storozhenko.dev>
2022-02-04 13:48:59 +0300

    new phrase moved to dictionary.toml

--- src/main/kotlin/space/yaroslav/familybot/executors/command/nonpublic/VestnikCommandExecutor.kt
+++ src/main/kotlin/space/yaroslav/familybot/executors/command/nonpublic/VestnikCommandExecutor.kt
@@ -8,6 +8,7 @@ import org.telegram.telegrambots.meta.bots.AbsSender
 import space.yaroslav.familybot.common.extensions.send
 import space.yaroslav.familybot.common.extensions.sendDeferred
 import space.yaroslav.familybot.executors.command.CommandExecutor
+import space.yaroslav.familybot.models.dictionary.Phrase
 import space.yaroslav.familybot.models.router.ExecutorContext
 import space.yaroslav.familybot.models.telegram.Chat
 import space.yaroslav.familybot.models.telegram.Command
@@ -38,7 +39,8 @@ class VestnikCommandExecutor(
                     }
                 }
             }
-            sender.send(context, "Случайный выпуск Вестника Кала:")
+
+            sender.send(context, context.phrase(Phrase.RANDOM_VESTNIK))
             sender.sendDeferred(context, question, shouldTypeBeforeSend = true)
         }
     }
