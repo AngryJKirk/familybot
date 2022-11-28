9aae5a6cec240eaa68add506cad99c521dbc9856
Yaroslav Storozhenko <yaroslav@storozhenko.dev>
2022-11-28 17:16:02 +0000

    /time optimization

--- src/main/kotlin/space/yaroslav/familybot/executors/command/nonpublic/TimeExecutor.kt
+++ src/main/kotlin/space/yaroslav/familybot/executors/command/nonpublic/TimeExecutor.kt
@@ -1,32 +1,25 @@
 package space.yaroslav.familybot.executors.command.nonpublic
 
-import com.fasterxml.jackson.annotation.JsonProperty
-import kotlinx.coroutines.*
-import org.springframework.http.HttpMethod
 import org.springframework.stereotype.Component
-import org.springframework.web.client.RestTemplate
 import org.telegram.telegrambots.meta.bots.AbsSender
 import space.yaroslav.familybot.common.extensions.bold
 import space.yaroslav.familybot.common.extensions.code
 import space.yaroslav.familybot.common.extensions.send
 import space.yaroslav.familybot.executors.command.CommandExecutor
-import space.yaroslav.familybot.executors.pm.PrivateMessageExecutor
 import space.yaroslav.familybot.models.router.ExecutorContext
 import space.yaroslav.familybot.models.telegram.Command
-import space.yaroslav.familybot.telegram.FamilyBot
-import java.time.LocalDateTime
+import java.time.Instant
+import java.time.ZoneId
 import java.time.format.DateTimeFormatter
 
 @Component
 class TimeExecutor : CommandExecutor() {
-    private val restTemplate = RestTemplate()
 
     private val inputDateTimeFormatter =
         DateTimeFormatter.ofPattern("HH:mm")
 
 
     companion object {
-        private const val timeZoneUrl = "https://worldtimeapi.org/api/timezone/"
         private val times = mapOf(
             "Время в Лондоне:    " to "Europe/London",
             "Время в Москве:     " to "Europe/Moscow",
@@ -34,39 +27,23 @@ class TimeExecutor : CommandExecutor() {
             "Время в Аргентине:  " to "America/Argentina/Buenos_Aires",
             "Время в Малайзии:   " to "Asia/Kuala_Lumpur",
         )
+            .map { (prefix, zone) -> prefix.code() to ZoneId.of(zone) }
+            .toMap()
     }
 
     override fun command() = Command.TIME
 
     override fun execute(context: ExecutorContext): suspend (AbsSender) -> Unit {
+        val now = Instant.now()
+        val result = times.map { (prefix, zone) -> prefix + getDateString(now, zone) }
+            .joinToString(separator = "\n")
         return {
-            val result =
-                times
-                    .map { (prefix, zone) -> prefix.code() to callApiAsync(zone) }
-                    .map { (prefix, result) -> prefix + result.await() }
-                    .joinToString(separator = "\n")
             it.send(context, result, replyToUpdate = true, enableHtml = true)
         }
     }
 
-    private suspend fun callApiAsync(zone: String): Deferred<String> {
-        return coroutineScope {
-            async {
-                runCatching {
-                    val response = withContext(Dispatchers.IO) {
-                        restTemplate
-                            .exchange(timeZoneUrl + zone, HttpMethod.GET, null, DateTimeServiceResponse::class.java)
-                    }
-                        .body ?: throw FamilyBot.InternalException("Can't deserialize response from date time API")
-
-                    LocalDateTime.parse(response.datetime, DateTimeFormatter.ISO_OFFSET_DATE_TIME)
-                        .format(inputDateTimeFormatter).bold()
-                }.getOrDefault("Абонент недоступен...")
-            }
-        }
+    private fun getDateString(now: Instant, zone: ZoneId): String {
+        return now.atZone(zone).format(inputDateTimeFormatter).bold()
     }
-
-    private class DateTimeServiceResponse(@JsonProperty("datetime") val datetime: String)
-
 
 }
