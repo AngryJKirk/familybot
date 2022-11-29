a6d6a21eb76c4910f44fc51683504a63ebe37af2
Yaroslav Storozhenko <yaroslav@storozhenko.dev>
2022-11-29 20:48:30 +0000

    time sort

--- src/main/kotlin/dev/storozhenko/familybot/executors/command/nonpublic/TimeExecutor.kt
+++ src/main/kotlin/dev/storozhenko/familybot/executors/command/nonpublic/TimeExecutor.kt
@@ -1,13 +1,13 @@
 package dev.storozhenko.familybot.executors.command.nonpublic
 
-import org.springframework.stereotype.Component
-import org.telegram.telegrambots.meta.bots.AbsSender
 import dev.storozhenko.familybot.common.extensions.bold
 import dev.storozhenko.familybot.common.extensions.code
 import dev.storozhenko.familybot.common.extensions.send
 import dev.storozhenko.familybot.executors.command.CommandExecutor
 import dev.storozhenko.familybot.models.router.ExecutorContext
 import dev.storozhenko.familybot.models.telegram.Command
+import org.springframework.stereotype.Component
+import org.telegram.telegrambots.meta.bots.AbsSender
 import java.time.Instant
 import java.time.ZoneId
 import java.time.format.DateTimeFormatter
@@ -15,7 +15,7 @@ import java.time.format.DateTimeFormatter
 @Component
 class TimeExecutor : CommandExecutor() {
 
-    private val inputDateTimeFormatter =
+    private val timeFormatter =
         DateTimeFormatter.ofPattern("HH:mm")
 
 
@@ -36,15 +36,16 @@ class TimeExecutor : CommandExecutor() {
 
     override fun execute(context: ExecutorContext): suspend (AbsSender) -> Unit {
         val now = Instant.now()
-        val result = times.map { (prefix, zone) -> prefix + getDateString(now, zone) }
-            .joinToString(separator = "\n")
+        val result = times.map { (prefix, zone) -> prefix to now.atZone(zone) }
+            .sortedBy { (_, time) -> time }
+            .joinToString(separator = "\n") { (prefix, time) -> prefix + time.format(timeFormatter).bold() }
         return {
             it.send(context, result, replyToUpdate = true, enableHtml = true)
         }
     }
 
     private fun getDateString(now: Instant, zone: ZoneId): String {
-        return now.atZone(zone).format(inputDateTimeFormatter).bold()
+        return now.atZone(zone).format(timeFormatter).bold()
     }
 
 }
