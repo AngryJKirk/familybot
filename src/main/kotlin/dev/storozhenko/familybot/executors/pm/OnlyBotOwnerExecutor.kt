5bba820608bd927c92edc9569ab404521aeb09b3
Yaroslav <yaroslav@storozhenko.dev>
2023-03-18 09:51:46 +0000

    imroved admin interaction

--- src/main/kotlin/dev/storozhenko/familybot/executors/pm/OnlyBotOwnerExecutor.kt
+++ src/main/kotlin/dev/storozhenko/familybot/executors/pm/OnlyBotOwnerExecutor.kt
@@ -1,9 +1,11 @@
 package dev.storozhenko.familybot.executors.pm
 
+import dev.storozhenko.familybot.getLogger
 import dev.storozhenko.familybot.models.router.ExecutorContext
 import dev.storozhenko.familybot.models.router.Priority
 import dev.storozhenko.familybot.telegram.BotConfig
-import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import org.telegram.telegrambots.meta.api.methods.updatingmessages.DeleteMessage
@@ -11,6 +13,9 @@ import org.telegram.telegrambots.meta.bots.AbsSender
 import kotlin.time.Duration.Companion.minutes
 
 abstract class OnlyBotOwnerExecutor(private val botConfig: BotConfig) : PrivateMessageExecutor {
+    companion object {
+        private val deleteMessageScope = CoroutineScope(Dispatchers.Default)
+    }
 
     override fun canExecute(context: ExecutorContext): Boolean {
         val message = context.message
@@ -28,16 +33,17 @@ abstract class OnlyBotOwnerExecutor(private val botConfig: BotConfig) : PrivateM
         return { sender ->
             val trackingAbsSender = TrackingAbsSender(sender)
             executeInternal(context).invoke(trackingAbsSender)
-            val idsToDelete = trackingAbsSender.tracking.apply { add(context.message) }
-            coroutineScope {
-                launch {
+            val idsToDelete = trackingAbsSender.tracking
+                .map { DeleteMessage(it.chat.id.toString(), it.messageId) }
+                .plus(DeleteMessage(context.chat.idString, context.message.messageId))
+
+            deleteMessageScope.launch {
+                runCatching {
                     if (context.testEnvironment.not()) {
                         delay(3.minutes)
-                        idsToDelete.forEach { message ->
-                            sender.execute(DeleteMessage(context.chat.idString, message.messageId))
-                        }
+                        idsToDelete.forEach(sender::execute)
                     }
-                }
+                }.onFailure { e -> getLogger().error("Failed to delete message", e) }
             }
         }
     }
