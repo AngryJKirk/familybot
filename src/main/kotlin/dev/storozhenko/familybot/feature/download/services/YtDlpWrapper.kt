2caca501658aba7671e7f3694db8c85a6e0fc613
Yaroslav Storozhenko <yaroslav@storozhenko.dev>
2023-12-14 22:07:12 +0000

    proper tmp file deletion

--- src/main/kotlin/dev/storozhenko/familybot/feature/download/services/YtDlpWrapper.kt
+++ src/main/kotlin/dev/storozhenko/familybot/feature/download/services/YtDlpWrapper.kt
@@ -3,13 +3,20 @@ package dev.storozhenko.familybot.feature.download.services
 import dev.storozhenko.familybot.BotConfig
 import dev.storozhenko.familybot.core.telegram.FamilyBot
 import dev.storozhenko.familybot.getLogger
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import org.springframework.stereotype.Component
 import java.io.File
 import java.util.UUID
+import kotlin.time.Duration.Companion.minutes
 
 @Component
 class YtDlpWrapper(private val botConfig: BotConfig) {
     private val log = getLogger()
+    private val fileDeleteScope = CoroutineScope(Dispatchers.Default)
+
     fun downloadVideo(url: String, vararg params: String): File {
         return download("${UUID.randomUUID()}.%(ext)s", url, *params)
     }
@@ -27,6 +34,18 @@ class YtDlpWrapper(private val botConfig: BotConfig) {
         process.inputStream.reader(Charsets.UTF_8).use { log.info(it.readText()) }
         process.waitFor()
         log.info("Finished running yt-dlp")
-        return folder.listFiles()?.firstOrNull() ?: throw FamilyBot.InternalException("yt-dlp failed to extract data")
+        val downloadedFile =
+            folder.listFiles()?.firstOrNull() ?: throw FamilyBot.InternalException("yt-dlp failed to extract data")
+        downloadedFile.deleteOnExit()
+        fileDeleteScope.launch {
+            runCatching {
+                delay(5.minutes)
+                downloadedFile.delete()
+                log.info("${downloadedFile.absolutePath} is deleted")
+            }.onFailure {
+                log.error("Could not delete ${downloadedFile.absolutePath}", it)
+            }
+        }
+        return downloadedFile
     }
 }
