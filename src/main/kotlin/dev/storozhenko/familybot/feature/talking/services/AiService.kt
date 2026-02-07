fe628484c5c8632651053e673cb54ba57ec6e03e
Yaroslav Storozhenko <yaroslav@storozhenko.dev>
2026-02-07 16:42:41 +0000

    download tg image directly

--- src/main/kotlin/dev/storozhenko/familybot/feature/talking/services/AiService.kt
+++ src/main/kotlin/dev/storozhenko/familybot/feature/talking/services/AiService.kt
@@ -15,14 +15,25 @@ import dev.storozhenko.familybot.BotConfig
 import dev.storozhenko.familybot.common.extensions.SenderLogger.log
 import dev.storozhenko.familybot.core.routers.models.ExecutorContext
 import dev.storozhenko.familybot.core.telegram.FamilyBot
+import okhttp3.OkHttpClient
+import okhttp3.Request
 import org.springframework.stereotype.Component
 import org.telegram.telegrambots.meta.api.methods.GetFile
+import java.time.Duration
+import kotlin.io.encoding.Base64
 import kotlin.time.Duration.Companion.seconds
 
 @Component
 class AiService(
     private val botConfig: BotConfig,
 ) {
+
+    private val client = OkHttpClient().newBuilder()
+        .connectTimeout(Duration.ofMinutes(1))
+        .readTimeout(Duration.ofMinutes(1))
+        .callTimeout(Duration.ofMinutes(1))
+        .retryOnConnectionFailure(true)
+        .build()
 
     suspend fun getImageDescription(
         context: ExecutorContext,
@@ -61,10 +72,23 @@ class AiService(
             if (fileId == null) return null
             val file = context.client.execute(GetFile(fileId))
             val url = file.getFileUrl(botConfig.botToken)
-            return if (message.hasPhoto()) url else null
+            val photoUrl = if (message.hasPhoto()) url else return null
+            return downloadImageAsBase64(photoUrl)
         } catch (e: Exception) {
             log.error(e) { "Can not download file" }
             return null
+        }
+    }
+
+    fun downloadImageAsBase64(url: String): String? {
+        val request = Request.Builder()
+            .url(url)
+            .build()
+
+        client.newCall(request).execute().use { response ->
+            if (!response.isSuccessful) return null
+            val imageBytes = response.body?.bytes() ?: return null
+            return Base64.encode(imageBytes)
         }
     }
 
